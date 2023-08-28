f5e018ca70a0035a2c9432a73ea4550234fa5c70
Pranav Anbarasu <pranavanba@gmail.com>
2023-08-28 23:01:54 +0000

    Calculate age from DoB

--- filtering.R
+++ filtering.R
@@ -1,3 +1,17 @@
+# Calculate age from DoB -----------------------------------------------------
+dob2age <- function(dataset, column, output=PARQUET_FILTERED_LOCATION) {
+  if (dataset %in% list.dirs(AWS_PARQUET_DOWNLOAD_LOCATION, full.names = F)) {
+    input_path <- paste0(AWS_PARQUET_DOWNLOAD_LOCATION, '/', dataset)
+    final_path <- paste0(output, '/', dataset, '/')
+    
+    arrow::open_dataset(sources = input_path) %>% 
+      dplyr::mutate(age = lubridate::year(lubridate::today())-lubridate::year(lubridate::as_date(!!sym(column)))) %>% 
+      arrow::write_dataset(path = input_path, max_rows_per_file = 900000)
+  }
+}
+
+dob2age("dataset_enrolledparticipants", "DateOfBirth")
+
 # Drop columns with potentially identifying info --------------------------
 drop_cols_datasets <- function(dataset, columns=c(), output=PARQUET_FILTERED_LOCATION) {
   if (dataset %in% list.dirs(AWS_PARQUET_DOWNLOAD_LOCATION, full.names = F)) {
