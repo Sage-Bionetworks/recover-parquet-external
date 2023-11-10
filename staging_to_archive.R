12b642e99107e3d242a470e41367d88bfddf51af
Pranav Anbarasu <pranavanba@gmail.com>
2023-11-10 21:10:29 +0000

    Get s3 URIs for staging and archive and replace staging params with prod params

--- staging_to_archive.R
+++ staging_to_archive.R
@@ -1,6 +1,11 @@
+base_s3_uri_staging <- paste0('s3://', PARQUET_BUCKET_EXTERNAL, '/', PARQUET_BUCKET_BASE_KEY_ARCHIVE)
+
 rm(list = names(config::get(config = "staging")))
 config::get(config = "prod") %>% list2env(envir = .GlobalEnv)
 
+base_s3_uri_archive <- paste0('s3://', PARQUET_BUCKET_EXTERNAL, '/', PARQUET_BUCKET_BASE_KEY_ARCHIVE)
+
 validated_date <- readline("Enter name of validated staging folder in yyyy-mm-dd format: ")
-cmd <- glue::glue("aws s3 --profile service-catalog cp {base_s3_uri_staging}/{validated_date} {base_s3_uri_archive}")
+cmd <- glue::glue("aws s3 --profile service-catalog cp {base_s3_uri_staging}{validated_date} {base_s3_uri_archive}")
 rm(validated_date)
+
