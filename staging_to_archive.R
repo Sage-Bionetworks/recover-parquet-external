a18ea1deec9cc73c558c3c7e675d6c83dfbdbc88
Pranav Anbarasu <pranavanba@gmail.com>
2023-11-10 21:21:02 +0000

    Get staging params directly from config file

--- staging_to_archive.R
+++ staging_to_archive.R
@@ -1,13 +1,22 @@
 library(synapser)
+library(tidyverse)
 
 synapser::synLogin(authToken = Sys.getenv('SYNAPSE_AUTH_TOKEN'))
 
-base_s3_uri_staging <- paste0('s3://', PARQUET_BUCKET_EXTERNAL, '/', PARQUET_BUCKET_BASE_KEY_ARCHIVE)
+base_s3_uri_staging <- 
+  paste0('s3://', 
+         config::get("PARQUET_BUCKET_EXTERNAL", "staging"), 
+         '/', 
+         config::get("PARQUET_BUCKET_BASE_KEY_ARCHIVE", "staging"))
 
 rm(list = names(config::get(config = "staging")))
 config::get(config = "prod") %>% list2env(envir = .GlobalEnv)
 
-base_s3_uri_archive <- paste0('s3://', PARQUET_BUCKET_EXTERNAL, '/', PARQUET_BUCKET_BASE_KEY_ARCHIVE)
+base_s3_uri_archive <- 
+  paste0('s3://', 
+         PARQUET_BUCKET_EXTERNAL, 
+         '/', 
+         PARQUET_BUCKET_BASE_KEY_ARCHIVE)
 
 validated_date <- readline("Enter name of validated staging folder in yyyy-mm-dd format: ")
 cmd <- glue::glue("aws s3 --profile service-catalog cp {base_s3_uri_staging}{validated_date} {base_s3_uri_archive} --exclude '*owner.txt*' --exclude '*archive*'")
